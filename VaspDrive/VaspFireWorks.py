83d2f9e36b402c10e708da01e72e5e3f5b853953
Bruno Rousseau <rousseau.bruno@gmail.com>
2015-06-01 11:24:24 -0400

    New FireTask class to generate and launch Vasp jobs.

--- VaspDrive/VaspFireWorks.py
+++ VaspDrive/VaspFireWorks.py
@@ -11,45 +11,21 @@ from fireworks.core.firework import FireTaskBase, FWAction
 from VaspSubmission import *
 import re
 
-class BuildVaspInputTask(FireTaskBase):
-    """ FireWork task which wraps around the generation of VASP inputs """
+job_template = """#!/bin/bash
 
-    required_params = ["structure", "workdir", "job_name"]
-    optional_params = ["nproc", "supplementary_incar_dict"]
+module purge
+module add intel/2013u0
+module add openmpi/1.6.3
 
-    _fw_name = 'BuildVaspInputTask'
+VASP=/gpfsFS1/scratch/nobackup/projets/gc029/opt/vasp/4.6/vtstNC_XC_2013/vasp
 
-    def run_task(self, fw_spec):
-        """ overload the run_task method to write the Vasp input """
-        self._load_params(self)
+source ~/.bash_profile
+source /gpfsFS1/scratch/nobackup/projets/gc029/xw2738/BASH/create_machinefile.sh
+create_machinefile
+unset SGE_ROOT
 
-        check = get_MaterialsProject_VASP_inputs(self.structure, self.workdir, self.job_name, 
-                                    nproc=self.nproc, supplementary_incar_dict = self.supplementary_incar_dict)
-
-    def _load_params(self, d):
-        """
-        Extract parameters from dictionary, imposing default value for optional parameters
-        if they are absent.
-        """
-
-        # It appears the structure gets serialized into a dictionary by the action of writing
-        # the specs. Let's give it life again!
-        self.structure = Structure.from_dict(d['structure'])
-
-        self.workdir = d['workdir']
-        self.job_name = d['job_name']
-
-        if 'nproc' in d:
-            self.nproc = int(d['nproc'])
-        else:
-            self.nproc = 16
-
-        if 'supplementary_incar_dict' in d:
-            self.supplementary_incar_dict = d['supplementary_incar_dict']
-        else:
-            self.supplementary_incar_dict = None
-
-
+time mpiexec -np $NBCORE -x LD_LIBRARY_PATH -machinefile machines $VASP &> output.txt
+"""
 
 class MyTestTask(FireTaskBase):
     """ Simple test task to play with Fireworks' various functionalities.
@@ -85,3 +61,132 @@ class MyTestTask(FireTaskBase):
         number = int(re.search(r'\d+', last_string).group())
 
         return number
+
+class BuildVaspInputTask(FireTaskBase):
+    """ FireWork task which wraps around the generation of VASP inputs """
+
+    required_params = ["structure", "workdir", "job_name"]
+    optional_params = ["nproc", "supplementary_incar_dict"]
+
+    _fw_name = 'BuildVaspInputTask'
+
+    def run_task(self, fw_spec):
+        """ overload the run_task method to write the Vasp input """
+        self._load_params(self)
+
+        check = get_MaterialsProject_VASP_inputs(self.structure, self.workdir, self.job_name, 
+                                    nproc=self.nproc, supplementary_incar_dict = self.supplementary_incar_dict)
+
+
+
+
+class MyVaspFireTask(FireTaskBase):
+    """
+    This task will write/hack the VASP inputs, so that they are ready to 
+    roll.
+    """
+
+    _fw_name = 'MyVaspFireTask'
+
+    def run_task(self, fw_spec):
+
+        launch_dir = fw_spec['_launch_dir']
+
+        self._load_params(fw_spec)
+
+        self.generate_VASP_inputs(self.structure, self.job_name, self.nproc, 
+                U_strategy_instance = self.U_strategy, supplementary_incar_dict = self.supplementary_incar_dict)
+
+        # execute the BASH script
+        os.system('bash job.sh')           
+
+
+
+    def _load_params(self, d):
+        """
+        Extract parameters from dictionary, imposing default value for optional parameters
+        if they are absent.
+        """
+
+        # It appears the structure gets serialized into a dictionary by the action of writing
+        # the specs. Let's give it life again!
+        self.structure = Structure.from_dict(d['structure'])
+        self.job_name = d['job_name']
+
+        if 'nproc' in d:
+            self.nproc = int(d['nproc'])
+        else:
+            self.nproc = 16
+
+        if 'supplementary_incar_dict' in d:
+            self.supplementary_incar_dict = d['supplementary_incar_dict']
+        else:
+            self.supplementary_incar_dict = None
+
+        if 'U_strategy' in d:
+            self.U_strategy = d['U_strategy']
+        else:
+            self.U_strategy = None
+        
+
+    def generate_VASP_inputs(self, structure, job_name, nproc=16, U_strategy_instance = None, supplementary_incar_dict = None):
+        """
+        Inputs will be inspired by MaterialsProject, but this function is appropriate
+        when we are seriously modifying the inputs such that they no longer conform to Materials Project.
+        """
+
+        # let's start with MP
+        input_set = MPVaspInputSet()
+       
+        incar  = input_set.get_incar(structure)
+
+        if U_strategy_instance != None:
+            #  reading the structure here insures consistency, rather
+            #  than having the strategy read the structure outside this driver.
+            U_strategy_instance.read_structure(structure)
+
+            # Generate all LDAU-related variables according to specified strategy.
+            LDAU_dict, poscar_need_hack, potcar_need_hack = U_strategy_instance.get_LDAU()
+
+            incar.update(LDAU_dict) 
+
+        # set the number of parallel processors to sqrt(nproc), 
+        # as recommended in manual.
+        incar.update({'NPAR':int(np.sqrt(nproc))}) 
+
+        if supplementary_incar_dict != None:
+            incar.update(supplementary_incar_dict) 
+
+        poscar = input_set.get_poscar(structure)
+        kpoints = input_set.get_kpoints(structure)
+        potcar = input_set.get_potcar(structure)
+
+        incar.write_file(workdir+'INCAR')
+        poscar.write_file(workdir+'POSCAR', vasp4_compatible = True)
+        kpoints.write_file(workdir+'KPOINTS')
+        potcar.write_file(workdir+'POTCAR')
+
+        if poscar_need_hack:
+            # do we need specialized hacking of the poscar because of the U strategy?       
+            # For instance, if two Transition Metal sites with the same element 
+            # must be treated as independent, Pymatgen is not capable of doing this
+            # and we must HACK.
+            new_poscar_lines = U_strategy_instance.get_new_poscar_lines()
+            with open('POSCAR','w') as f:
+                for line in new_poscar_lines:
+                    print >>f, line.strip()
+
+        if potcar_need_hack:
+            # do we need specialized hacking of the potcar because of the U strategy?       
+            new_potcar_symbols = U_strategy_instance.get_new_potcar_symbols(potcar)
+            new_potcar = Potcar(new_potcar_symbols) 
+            # overwrite the previous potcar
+            new_potcar.write_file(workdir+'POTCAR')
+
+        with open('job.sh','w') as f:
+            f.write(job_template)
+
+        with open('clean.sh','w') as f:
+            f.write(clean_template)
+
+        return 0
